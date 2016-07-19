d9a4cf6bbe54c8e4dcdb9d89293ba146f6a0a078
Jalen <jalen@kuzcolighting.com>
2016-07-19 11:35:52 -0700

    Add MediaTypes for UTF_8 support

---
+++ itdoes-common-jar/src/main/java/com/itdoes/common/web/MediaTypes.java
@@ -0,0 +1,27 @@
+package com.itdoes.common.web;
+
+/**
+ * @author Jalen Zhong
+ */
+public interface MediaTypes {
+	String APPLICATION_XML = "application/xml";
+	String APPLICATION_XML_UTF_8 = "application/xml; charset=UTF-8";
+
+	String APPLICATION_JSON = "application/json";
+	String APPLICATION_JSON_UTF_8 = "application/json; charset=UTF-8";
+
+	String APPLICATION_JAVASCRIPT = "application/javascript";
+	String APPLICATION_JAVASCRIPT_UTF_8 = "application/javascript; charset=UTF-8";
+
+	String APPLICATION_XHTML_XML = "application/xhtml+xml";
+	String APPLICATION_XHTML_XML_UTF_8 = "application/xhtml+xml; charset=UTF-8";
+
+	String TEXT_PLAIN = "text/plain";
+	String TEXT_PLAIN_UTF_8 = "text/plain; charset=UTF-8";
+
+	String TEXT_XML = "text/xml";
+	String TEXT_XML_UTF_8 = "text/xml; charset=UTF-8";
+
+	String TEXT_HTML = "text/html";
+	String TEXT_HTML_UTF_8 = "text/html; charset=UTF-8";
+}
