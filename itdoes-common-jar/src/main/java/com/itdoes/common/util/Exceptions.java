6abb4ca78c14dac5e68dcb4684a1bf8648429031
Jalen <jalen@kuzcolighting.com>
2016-07-20 09:35:22 -0700

    Complete Exceptions util

--- itdoes-common-jar/src/main/java/com/itdoes/common/util/Exceptions.java
+++ itdoes-common-jar/src/main/java/com/itdoes/common/util/Exceptions.java
@@ -1,5 +1,8 @@
 package com.itdoes.common.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * @author Jalen Zhong
  */
@@ -12,11 +15,44 @@ public class Exceptions {
 		}
 	}
 
-    public static String getStackTraceString(Throwable t){
-        final StringWriter stringWriter = new StringWriter();
-        t.printStackTrace(new PrintWriter(stringWriter));
-        return stringWriter.toString();
-    }
+	public static String getStackTraceAsString(Throwable t) {
+		final StringWriter stringWriter = new StringWriter();
+		t.printStackTrace(new PrintWriter(stringWriter));
+		return stringWriter.toString();
+	}
+
+	public static String getErrorMessageWithNestedException(Throwable t) {
+		Throwable nestedException = t.getCause();
+		if (nestedException == null) {
+			return t.getMessage();
+		}
+
+		return new StringBuilder().append(t.getMessage()).append(" nested exception is ")
+				.append(nestedException.getClass().getName()).append(":").append(nestedException.getMessage())
+				.toString();
+	}
+
+	public static Throwable getRootCause(Throwable t) {
+		Throwable cause;
+		while ((cause = t.getCause()) != null) {
+			t = cause;
+		}
+		return t;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static boolean isCausedBy(Throwable t, Class<? extends Exception>... causeExceptionClasses) {
+		Throwable cause = t;
+		while (cause != null) {
+			for (Class<? extends Exception> causeExceptionClass : causeExceptionClasses) {
+				if (causeExceptionClass.isInstance(cause)) {
+					return true;
+				}
+			}
+			cause = cause.getCause();
+		}
+		return false;
+	}
 
 	private Exceptions() {
 	}
