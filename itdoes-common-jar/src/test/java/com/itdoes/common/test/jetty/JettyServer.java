bad22f81d35d2844b3780d27a5b6ab8e54ff6fce
Jalen <jalen@kuzcolighting.com>
2016-07-19 22:02:23 -0700

    Upgrade jetty from 8 to 9

--- itdoes-common-jar/src/test/java/com/itdoes/common/test/jetty/JettyServer.java
+++ itdoes-common-jar/src/test/java/com/itdoes/common/test/jetty/JettyServer.java
@@ -3,9 +3,8 @@ package com.itdoes.common.test.jetty;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
@@ -19,7 +18,7 @@ import com.itdoes.common.util.Exceptions;
  */
 public class JettyServer {
 	private static final String DEFAULT_WEBAPP_PATH = "src/main/webapp";
-	private static final String WEBDEFAULT_WINDOWS_PATH = "jetty/webdefault-windows.xml";
+	private static final String WEBDEFAULT_PATH = "jetty/webdefault-me.xml";
 
 	private static Logger logger = LoggerFactory.getLogger(JettyServer.class);
 
@@ -92,13 +91,13 @@ public class JettyServer {
 	private void init() {
 		server.setStopAtShutdown(true);
 
-		SelectChannelConnector connector = new SelectChannelConnector();
+		ServerConnector connector = new ServerConnector(server);
 		connector.setPort(port);
 		connector.setReuseAddress(false);
-		server.setConnectors(new Connector[] { connector });
+		server.addConnector(connector);
 
 		WebAppContext context = new WebAppContext(DEFAULT_WEBAPP_PATH, contextPath);
-		context.setDefaultsDescriptor(WEBDEFAULT_WINDOWS_PATH);
+		context.setDefaultsDescriptor(WEBDEFAULT_PATH);
 		server.setHandler(context);
 	}
 }
