35a6a985fbf33cdb768353f7ce1999a70a271f45
Jalen <jalen@kuzcolighting.com>
2016-07-21 09:19:09 -0700

    Add GreenMailServer test

---
+++ itdoes-common-jar/src/test/java/com/itdoes/common/test/greenmail/GreenMailServerTest.java
@@ -0,0 +1,24 @@
+package com.itdoes.common.test.greenmail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.itdoes.common.test.spring.SpringTestCase;
+
+/**
+ * @author Jalen Zhong
+ */
+@ContextConfiguration(locations = { "/applicationContext-mail.xml" })
+public class GreenMailServerTest extends SpringTestCase {
+	@Autowired
+	private GreenMail greenMail;
+
+	@Test
+	public void greenMail() {
+		assertThat(greenMail.getSmtp().getPort()).isEqualTo(GreenMailServer.DEFAULT_PORT);
+	}
+}
