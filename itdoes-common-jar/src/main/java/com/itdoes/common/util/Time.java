1cd4c0db4b2c88a5f8e6f96502866dead5f31f5a
Jalen <jalen@kuzcolighting.com>
2016-07-20 23:00:04 -0700

    Add Time util

---
+++ itdoes-common-jar/src/main/java/com/itdoes/common/util/Time.java
@@ -0,0 +1,64 @@
+package com.itdoes.common.util;
+
+import java.util.Date;
+
+/**
+ * @author Jalen Zhong
+ */
+public interface Time {
+	Time NOW = new NowTime();
+
+	Date getDate();
+
+	long getTime();
+
+	class NowTime implements Time {
+		@Override
+		public Date getDate() {
+			return new Date();
+		}
+
+		@Override
+		public long getTime() {
+			return System.currentTimeMillis();
+		}
+	}
+
+	class MockTime implements Time {
+		private long time;
+
+		public MockTime() {
+			this(0);
+		}
+
+		public MockTime(Date date) {
+			this(date.getTime());
+		}
+
+		public MockTime(long time) {
+			this.time = time;
+		}
+
+		@Override
+		public Date getDate() {
+			return new Date(time);
+		}
+
+		@Override
+		public long getTime() {
+			return time;
+		}
+
+		public void setTime(long time) {
+			this.time = time;
+		}
+
+		public void increase(int millis) {
+			time += millis;
+		}
+
+		public void decrease(int millis) {
+			time -= millis;
+		}
+	}
+}
