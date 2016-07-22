94bccc61c80bb4b1fc216543014963522c409daf
Jalen <jalen@kuzcolighting.com>
2016-07-22 14:23:22 -0700

    Add SqlExecutors for executing sql files

---
+++ itdoes-common-jar/src/test/java/com/itdoes/common/test/sql/SqlExecutors.java
@@ -0,0 +1,38 @@
+package com.itdoes.common.test.sql;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.jdbc.datasource.init.ScriptException;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import com.itdoes.common.util.Exceptions;
+
+/**
+ * @author Jalen Zhong
+ */
+public class SqlExecutors {
+	private static final String DEFAULT_ENCODING = "UTF-8";
+
+	private static final ResourceLoader RESOURCE_LOADER = new DefaultResourceLoader();
+
+	public static void executeSql(DataSource dataSource, String... sqlPaths) {
+		try {
+			for (String sqlPath : sqlPaths) {
+				final Resource resource = RESOURCE_LOADER.getResource(sqlPath);
+				ScriptUtils.executeSqlScript(dataSource.getConnection(),
+						new EncodedResource(resource, DEFAULT_ENCODING));
+			}
+		} catch (ScriptException | SQLException e) {
+			throw Exceptions.unchecked(e);
+		}
+	}
+
+	private SqlExecutors() {
+	}
+}
