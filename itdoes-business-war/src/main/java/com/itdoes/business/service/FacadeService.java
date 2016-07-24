7405301266f5feacf27c5eaea397e602cf498c83
Jalen <jalen@kuzcolighting.com>
2016-07-24 00:03:28 -0700

    Add initial create/update/delete/get/search for facade case

--- itdoes-business-war/src/main/java/com/itdoes/business/service/FacadeService.java
+++ itdoes-business-war/src/main/java/com/itdoes/business/service/FacadeService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itdoes.business.entity.BaseEntity;
 import com.itdoes.business.repository.BaseDao;
@@ -21,28 +22,39 @@ import com.itdoes.common.jpa.Specifications;
  */
 @Service
 public class FacadeService extends BaseService implements ApplicationContextAware {
-	private Map<Class<? extends BaseEntity>, BaseDao<? extends BaseEntity>> daoMap;
+	private Map daoMap;
 
 	// @Autowired
 	// private InvCompanyDao invCompanyDao;
 
 	private ApplicationContext applicationContext;
 
-	@SuppressWarnings("unchecked")
 	@PostConstruct
 	public void init() {
 		// daoMap.put(InvCompany.class, invCompanyDao);
 		daoMap = Businesses.getDaoMap(BaseEntity.class, BaseDao.class, applicationContext);
 	}
 
-	@SuppressWarnings("unchecked")
-	public List<? extends BaseEntity> getAll(Class<? extends BaseEntity> entityClass, List<SearchFilter> filters) {
+	public Object get(Class entityClass, Integer id) {
+		return getDao(entityClass).findOne(id);
+	}
+
+	public List<? extends BaseEntity> getAll(Class entityClass, List<SearchFilter> filters) {
 		return getDao(entityClass).findAll(Specifications.build(entityClass, filters));
 	}
 
-	@SuppressWarnings("rawtypes")
-	private BaseDao getDao(Class<? extends BaseEntity> entityClass) {
-		final BaseDao<? extends BaseEntity> baseDao = daoMap.get(entityClass);
+	@Transactional(readOnly = false)
+	public void save(Object entity) {
+		getDao(entity.getClass()).save(entity);
+	}
+
+	@Transactional(readOnly = false)
+	public void delete(Class entityClass, Integer id) {
+		getDao(entityClass).delete(id);
+	}
+
+	private BaseDao getDao(Class entityClass) {
+		final BaseDao baseDao = (BaseDao) daoMap.get(entityClass);
 		if (baseDao == null) {
 			throw new IllegalStateException("Dao cannot be found by entity class: " + entityClass);
 		}
--- itdoes-business-war/src/main/java/com/itdoes/business/web/FacadeController.java
+++ itdoes-business-war/src/main/java/com/itdoes/business/web/FacadeController.java
@@ -2,7 +2,11 @@ package com.itdoes.business.web;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,8 +29,40 @@ public class FacadeController extends BaseController {
 	@RequestMapping(value = "search", method = RequestMethod.GET)
 	public Result search(@RequestParam("filter") String filter) {
 		final FilterWrapper filterWrapper = parseFilter(filter);
-		List list = facadeService.getAll(filterWrapper.entityClass, filterWrapper.filters);
+		final List list = facadeService.getAll(filterWrapper.entityClass, filterWrapper.filters);
 		return Result.success(list.toArray(new Object[list.size()]));
+	}
+
+	@RequestMapping(value = "get", method = RequestMethod.GET)
+	public Result get(@RequestParam(value = "entityClass") Class entityClass, @RequestParam("id") Integer id) {
+		final Object entity = facadeService.get(entityClass, id);
+		return null;
+	}
+
+	@RequestMapping(value = "delete")
+	public Result delete(@RequestParam(value = "entityClass") Class entityClass, @RequestParam("id") Integer id) {
+		facadeService.delete(entityClass, id);
+		return null;
+	}
+
+	@RequestMapping(value = "create", method = RequestMethod.POST)
+	public Result create(@Valid Object entity) {
+		facadeService.save(entity);
+		return null;
+	}
+
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public Result update(@Valid @ModelAttribute("entity") Object entity) {
+		facadeService.save(entity);
+		return null;
+	}
+
+	@ModelAttribute
+	public void getEntity(@RequestParam(value = "entityClass") Class entityClass,
+			@RequestParam(value = "id", defaultValue = "-1") Integer id, Model model) {
+		if (id != -1) {
+			model.addAttribute("entity", facadeService.get(entityClass, id));
+		}
 	}
 
 	private FilterWrapper parseFilter(String filter) {
