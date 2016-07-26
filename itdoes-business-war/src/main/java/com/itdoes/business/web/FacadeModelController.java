2c361ee0f0bf29adcb319a1e083a98141501d9ec
Jalen <jalen@kuzcolighting.com>
2016-07-25 23:21:29 -0700

    Add custom date converter for facade controller

--- itdoes-business-war/src/main/java/com/itdoes/business/web/FacadeModelController.java
+++ itdoes-business-war/src/main/java/com/itdoes/business/web/FacadeModelController.java
@@ -1,9 +1,15 @@
 package com.itdoes.business.web;
 
+import java.beans.PropertyEditorSupport;
+import java.util.Date;
+
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +24,20 @@ import com.itdoes.common.web.MediaTypes;
 
 /**
  * @author Jalen Zhong
+ * 
+ *         <pre>
+ *         Two ways for Date<->String convert:
+ *         1) Add in Entity field
+ *         &#64;DateTimeFormat(pattern = "yyyy-MM-dd") 
+ *          
+ *         2) Add in Controller
+ *         &#64;InitBinder
+ *         public void initBinder(WebDataBinder binder) {
+ *         	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+ *         	dateFormat.setLenient(false);
+ *         	binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+ *         }
+ *         </pre>
  */
 @RestController
 @RequestMapping(value = "/facade", produces = MediaTypes.APPLICATION_JSON_UTF_8)
@@ -43,8 +63,27 @@ public class FacadeModelController extends BaseController {
 		if (id != null) {
 			model.addAttribute("entity", facadeService.get(ec, id));
 		} else {
-			BaseEntity be = facadeService.newInstance(ec);
 			model.addAttribute("entity", facadeService.newInstance(ec));
 		}
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String value) {
+				if (StringUtils.isBlank(value)) {
+					setValue(null);
+				} else {
+					setValue(new Date(Long.valueOf(value)));
+				}
+			}
+
+			@Override
+			public String getAsText() {
+				final Date date = (Date) getValue();
+				return date != null ? String.valueOf(date.getTime()) : "";
+			}
+		});
 	}
 }
