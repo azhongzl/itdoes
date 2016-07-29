8633419fc5b41394dadbf9a3ef260ec534e6871e
Jalen <jalen@kuzcolighting.com>
2016-07-29 16:20:48 -0700

    Add initial version of JaxbMapper test

--- itdoes-common-jar/src/test/java/com/itdoes/common/jaxb/JaxbMapperTest.java
+++ itdoes-common-jar/src/test/java/com/itdoes/common/jaxb/JaxbMapperTest.java
@@ -3,6 +3,8 @@ package com.itdoes.common.jaxb;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
@@ -10,6 +12,9 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.dom4j.Document;
@@ -19,6 +24,8 @@ import org.dom4j.Element;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.itdoes.common.jaxb.JaxbMapperTest.HouseMapAdapter.HouseMap.HouseEntry;
 
 /**
  * @author Jalen Zhong
@@ -96,12 +103,15 @@ public class JaxbMapperTest {
 	}
 
 	@XmlRootElement
-	@XmlType(propOrder = { "name", "interests" })
-	private static class User {
+	@XmlType(propOrder = { "name", "roles", "interests", "houses" })
+	public static class User {
 		private Long id;
 		private String name;
 		private String password;
+
+		private List<Role> roles = Lists.newArrayList();
 		private List<String> interests = Lists.newArrayList();
+		private Map<String, String> houses = Maps.newHashMap();
 
 		@XmlAttribute
 		public Long getId() {
@@ -112,7 +122,6 @@ public class JaxbMapperTest {
 			this.id = id;
 		}
 
-		@SuppressWarnings("unused")
 		public String getName() {
 			return name;
 		}
@@ -126,9 +135,18 @@ public class JaxbMapperTest {
 			return password;
 		}
 
-		@SuppressWarnings("unused")
 		public void setPassword(String password) {
 			this.password = password;
+		}
+
+		@XmlElementWrapper
+		@XmlElement(name = "role")
+		public List<Role> getRoles() {
+			return roles;
+		}
+
+		public void setRoles(List<Role> roles) {
+			this.roles = roles;
 		}
 
 		@XmlElementWrapper(name = "interests")
@@ -137,14 +155,100 @@ public class JaxbMapperTest {
 			return interests;
 		}
 
-		@SuppressWarnings("unused")
 		public void setInterests(List<String> interests) {
 			this.interests = interests;
+		}
+
+		@XmlJavaTypeAdapter(HouseMapAdapter.class)
+		public Map<String, String> getHouses() {
+			return houses;
+		}
+
+		public void setHouses(Map<String, String> houses) {
+			this.houses = houses;
 		}
 
 		@Override
 		public String toString() {
 			return ToStringBuilder.reflectionToString(this);
+		}
+	}
+
+	public static class Role {
+		private Long id;
+		private String name;
+
+		public Role() {
+		}
+
+		public Role(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@XmlAttribute
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		@XmlAttribute
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return ToStringBuilder.reflectionToString(this);
+		}
+	}
+
+	public static class HouseMapAdapter extends XmlAdapter<HouseMapAdapter.HouseMap, Map<String, String>> {
+		@Override
+		public HouseMap marshal(Map<String, String> map) throws Exception {
+			HouseMap houseMap = new HouseMap();
+			for (Entry<String, String> e : map.entrySet()) {
+				houseMap.entries.add(new HouseEntry(e));
+			}
+			return houseMap;
+		}
+
+		@Override
+		public Map<String, String> unmarshal(HouseMap houseMap) throws Exception {
+			Map<String, String> map = Maps.newLinkedHashMap();
+			for (HouseEntry e : houseMap.entries) {
+				map.put(e.key, e.value);
+			}
+			return map;
+		}
+
+		@XmlType(name = "houses")
+		public static class HouseMap {
+			@XmlElement(name = "house")
+			private List<HouseEntry> entries = Lists.newArrayList();
+
+			static class HouseEntry {
+				@XmlAttribute
+				private String key;
+
+				@XmlValue
+				private String value;
+
+				public HouseEntry() {
+				}
+
+				public HouseEntry(Entry<String, String> e) {
+					key = e.getKey();
+					value = e.getValue();
+				}
+			}
 		}
 	}
 }
