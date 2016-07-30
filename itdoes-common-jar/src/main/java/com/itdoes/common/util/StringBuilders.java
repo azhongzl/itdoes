e9896e57f0960a5db0600f2eccd80b5aab321038
Jalen <jalen@kuzcolighting.com>
2016-07-29 23:31:50 -0700

    Add StringBuilders

---
+++ itdoes-common-jar/src/main/java/com/itdoes/common/util/StringBuilders.java
@@ -0,0 +1,13 @@
+package com.itdoes.common.util;
+
+/**
+ * @author Jalen Zhong
+ */
+public class StringBuilders {
+	public static void reset(StringBuilder sb) {
+		sb.setLength(0);
+	}
+
+	private StringBuilders() {
+	}
+}
