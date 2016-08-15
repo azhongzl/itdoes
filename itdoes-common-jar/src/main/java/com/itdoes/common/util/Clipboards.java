a741ac609e284e2b8760456109d1ce4a096b06d4
Jalen <jalen@kuzcolighting.com>
2016-08-15 11:10:00 -0700

    Enhance Clipboards to retry if copy/paste fail

--- itdoes-common-jar/src/main/java/com/itdoes/common/util/Clipboards.java
+++ itdoes-common-jar/src/main/java/com/itdoes/common/util/Clipboards.java
@@ -7,12 +7,50 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Jalen Zhong
  */
 public class Clipboards {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Clipboards.class);
+
+	public static void copy(String content, int retryTimes) {
+		int retryNo = 0;
+		while (true) {
+			try {
+				copy(content);
+				break;
+			} catch (IllegalStateException e) {
+				retryNo++;
+				if (retryNo > retryTimes) {
+					throw e;
+				} else {
+					LOGGER.warn("Copy to clipboard fail, retry times is: {}/{}", retryNo, retryTimes, e);
+				}
+			}
+		}
+	}
+
 	public static void copy(String content) {
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(content), null);
+	}
+
+	public static String paste(int retryTimes) {
+		int retryNo = 0;
+		while (true) {
+			try {
+				return paste();
+			} catch (IllegalStateException e) {
+				retryNo++;
+				if (retryNo > retryTimes) {
+					throw e;
+				} else {
+					LOGGER.warn("Paste from clipboard fail, retry times is: {}/{}", retryNo, retryTimes, e);
+				}
+			}
+		}
 	}
 
 	public static String paste() {
