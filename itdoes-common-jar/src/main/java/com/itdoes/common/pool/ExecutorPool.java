25b88f5052dc6499e1c78470ecefdfcc454e654d
Jalen <jalen@kuzcolighting.com>
2016-08-16 15:31:54 -0700

    Add ExecutorPool which uses apache commons pool2

---
+++ itdoes-common-jar/src/main/java/com/itdoes/common/pool/ExecutorPool.java
@@ -0,0 +1,60 @@
+package com.itdoes.common.pool;
+
+import org.apache.commons.pool2.PooledObjectFactory;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import com.itdoes.common.util.Exceptions;
+
+/**
+ * @author Jalen Zhong
+ */
+public class ExecutorPool<T> extends GenericObjectPool<T> {
+	public ExecutorPool(PooledObjectFactory<T> factory, GenericObjectPoolConfig config) {
+		super(factory, config);
+	}
+
+	public <V> V execute(PoolCaller<T, V> caller, long borrowMaxWaitMillis) {
+		T t = null;
+		try {
+			t = borrowObject(borrowMaxWaitMillis);
+			return caller.call(t);
+		} catch (Exception e) {
+			throw Exceptions.unchecked(e);
+		} finally {
+			if (t != null) {
+				returnObject(t);
+			}
+		}
+	}
+
+	public <V> V execute(PoolCaller<T, V> caller) {
+		return execute(caller, getMaxWaitMillis());
+	}
+
+	public void execute(PoolRunner<T> runner, long borrowMaxWaitMillis) {
+		T t = null;
+		try {
+			t = borrowObject(borrowMaxWaitMillis);
+			runner.run(t);
+		} catch (Exception e) {
+			throw Exceptions.unchecked(e);
+		} finally {
+			if (t != null) {
+				returnObject(t);
+			}
+		}
+	}
+
+	public void execute(PoolRunner<T> runner) {
+		execute(runner, getMaxWaitMillis());
+	}
+
+	public interface PoolCaller<T, V> {
+		V call(T t);
+	}
+
+	public interface PoolRunner<T> {
+		void run(T t);
+	}
+}
