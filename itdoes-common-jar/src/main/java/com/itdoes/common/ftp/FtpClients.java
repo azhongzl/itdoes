9f73eaa451d0f9ce8c7a83a9b7bc3af93e3a0a60
Jalen <jalen@kuzcolighting.com>
2016-08-24 16:00:32 -0700

    Add initial version for makeDirectory of FtpClients

--- itdoes-common-jar/src/main/java/com/itdoes/common/ftp/FtpClients.java
+++ itdoes-common-jar/src/main/java/com/itdoes/common/ftp/FtpClients.java
@@ -8,15 +8,54 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.Validate;
 import org.apache.commons.net.ftp.FTPClient;
 
+import com.itdoes.common.util.Collections3;
 import com.itdoes.common.util.Exceptions;
+import com.itdoes.common.util.Files;
 
 /**
  * @author Jalen Zhong
  */
 public class FtpClients {
+	public static boolean makeDirectory(FTPClient client, String baseDir, String relativeDirs) throws IOException {
+		return makeDirectory(client, baseDir, relativeDirs, true);
+	}
+
+	public static boolean makeDirectory(FTPClient client, String baseDir, String relativeDirs, boolean createIfNotExist)
+			throws IOException {
+		Validate.notNull(client, "FTPClient is null");
+		Validate.notNull(baseDir, "BaseDir is null");
+		Validate.notNull(relativeDirs, "RelativeDirs is null");
+
+		final String relativeDirsStr = Files.toUnixPath(relativeDirs);
+
+		if (!createIfNotExist) {
+			return client.makeDirectory(relativeDirsStr);
+		}
+
+		final String[] myRemoteRelativeDirs = relativeDirsStr.split("/");
+		if (Collections3.isEmpty(myRemoteRelativeDirs)) {
+			return true;
+		}
+
+		String dir = Files.toUnixPath(baseDir);
+		System.out.println("----------------------------working dir-------------------------" + dir);
+		for (String myRemoteRelativeDir : myRemoteRelativeDirs) {
+			dir += "/" + myRemoteRelativeDir;
+			System.out.println("-----------------------------------------------------" + relativeDirs + "===" + dir);
+			client.makeDirectory(dir);
+		}
+
+		return true;
+	}
+
 	public static boolean retrieveFile(FTPClient client, String remote, String local) throws IOException {
+		Validate.notNull(client, "FTPClient is null");
+		Validate.notNull(remote, "Remote is null");
+		Validate.notNull(local, "Local is null");
+
 		OutputStream localOutput = null;
 		try {
 			localOutput = new FileOutputStream(local);
@@ -29,6 +68,10 @@ public class FtpClients {
 	}
 
 	public static boolean storeFile(FTPClient client, String remote, String local) throws IOException {
+		Validate.notNull(client, "FTPClient is null");
+		Validate.notNull(remote, "Remote is null");
+		Validate.notNull(local, "Local is null");
+
 		InputStream localInput = null;
 		try {
 			localInput = new FileInputStream(local);
@@ -40,18 +83,18 @@ public class FtpClients {
 		}
 	}
 
-	public static void close(FTPClient ftp) {
-		if (ftp == null) {
+	public static void close(FTPClient client) {
+		if (client == null) {
 			return;
 		}
 
 		try {
-			ftp.logout();
+			client.logout();
 		} catch (IOException e) {
 		}
 
 		try {
-			ftp.disconnect();
+			client.disconnect();
 		} catch (IOException e) {
 		}
 	}
