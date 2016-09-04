caa2d0483a66fe9a8547dfc7f88302cee58f6f6f
Jalen <jalen@kuzcolighting.com>
2016-09-03 19:23:38 -0700

    Add java keywords in Constants

--- itdoes-common-jar/src/main/java/com/itdoes/common/core/Constants.java
+++ itdoes-common-jar/src/main/java/com/itdoes/common/core/Constants.java
@@ -1,6 +1,8 @@
 package com.itdoes.common.core;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Jalen Zhong
@@ -8,4 +10,10 @@ import java.nio.charset.Charset;
 public interface Constants {
 	String UTF8 = "UTF-8";
 	Charset UTF8_CHARSET = Charset.forName(UTF8);
+
+	List<String> JAVA_KEYWORDS = Arrays.asList("abstract", "assert", "boolean", "break", "byte", "case", "catch",
+			"char", "class", "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally",
+			"float", "for", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new",
+			"package", "private", "protected", "public", "return", "strictfp", "short", "static", "super", "switch",
+			"synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while");
 }
