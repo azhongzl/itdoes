8be27c6ff0bbe454e92a74b95a05302ace9acde6
Jalen <jalen@kuzcolighting.com>
2016-09-04 00:22:00 -0700

    Remove duplicated code for EntityGeneratorHelper

--- itdoes-common-jar/src/main/java/com/itdoes/common/extension/codegenerator/java/EntityGeneratorHelper.java
+++ itdoes-common-jar/src/main/java/com/itdoes/common/extension/codegenerator/java/EntityGeneratorHelper.java
@@ -16,14 +16,16 @@ import com.itdoes.common.core.util.PropertiesLoader;
 public class EntityGeneratorHelper {
 	private static final String OUTPUT_DIR = "/codegenerator/java";
 	private static final String PROPERTY_FILE_PREFIX = "classpath:/extension/codegenerator/java/";
+	private static final String TABLE_MAPPING_PROPERTY_FILE = PROPERTY_FILE_PREFIX + "table.mapping.properties";
+	private static final String COLUMN_MAPPING_PROPERTY_FILE = PROPERTY_FILE_PREFIX + "column.mapping.properties";
+	private static final String COLUMN_SECURE_MAPPING_PROPERTY_FILE = PROPERTY_FILE_PREFIX
+			+ "column.secure.mapping.properties";
 
 	public static void generateEntities(String basePackageName, String idGeneratedValue) {
 		final PropertiesLoader pl = new PropertiesLoader("classpath:/application.properties",
 				"classpath:/application.local.properties");
-		final Map<String, String> tableMapping = Collections3
-				.toMap(new PropertiesLoader(PROPERTY_FILE_PREFIX + "table.mapping.properties").getProperties());
-		final Map<String, String> columnMapping = Collections3
-				.toMap(new PropertiesLoader(PROPERTY_FILE_PREFIX + "column.mapping.properties").getProperties());
+		final Map<String, String> tableMapping = toMap(TABLE_MAPPING_PROPERTY_FILE);
+		final Map<String, String> columnMapping = toMap(COLUMN_MAPPING_PROPERTY_FILE);
 		final List<String> secureColumnList = getSecureColumnList();
 
 		EntityGenerator.generateEntities(pl.getProperty("jdbc.driver"), pl.getProperty("jdbc.url"),
@@ -33,20 +35,23 @@ public class EntityGeneratorHelper {
 
 	private static List<String> getSecureColumnList() {
 		final List<String> secureColumnList = Lists.newArrayList();
-		final Map<String, String> secureColumnMapping = Collections3
-				.toMap(new PropertiesLoader(PROPERTY_FILE_PREFIX + "column.secure.mapping.properties").getProperties());
+		final Map<String, String> secureColumnMapping = toMap(COLUMN_SECURE_MAPPING_PROPERTY_FILE);
 		for (Entry<String, String> entry : secureColumnMapping.entrySet()) {
 			final String tableName = entry.getKey();
-			final String columnsStr = entry.getValue();
-			if (StringUtils.isNotBlank(columnsStr)) {
-				final String[] columns = StringUtils.split(columnsStr, ",");
-				if (!Collections3.isEmpty(columns)) {
-					for (String column : columns) {
-						secureColumnList.add(tableName + "." + column);
+			final String columnNamesStr = entry.getValue();
+			if (StringUtils.isNotBlank(columnNamesStr)) {
+				final String[] columnNames = StringUtils.split(columnNamesStr, ",");
+				if (!Collections3.isEmpty(columnNames)) {
+					for (String columnName : columnNames) {
+						secureColumnList.add(tableName + "." + columnName);
 					}
 				}
 			}
 		}
 		return secureColumnList;
+	}
+
+	private static Map<String, String> toMap(String propertyFilename) {
+		return Collections3.toMap(new PropertiesLoader(propertyFilename).getProperties());
 	}
 }
