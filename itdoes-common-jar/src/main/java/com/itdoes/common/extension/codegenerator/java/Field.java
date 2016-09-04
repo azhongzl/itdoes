3f03b16e0cc3ae2cded9f667c18ceae9c17b4a2d
Jalen <jalen@kuzcolighting.com>
2016-09-03 19:39:41 -0700

    Verify that a field name cannot be blank and java keyword, and type cannot be blank as well

--- itdoes-common-jar/src/main/java/com/itdoes/common/extension/codegenerator/java/Field.java
+++ itdoes-common-jar/src/main/java/com/itdoes/common/extension/codegenerator/java/Field.java
@@ -1,6 +1,9 @@
 package com.itdoes.common.extension.codegenerator.java;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+
+import com.itdoes.common.core.Constants;
 
 /**
  * @author Jalen Zhong
@@ -11,6 +14,13 @@ public class Field {
 	private final String type;
 
 	public Field(String name, String type) {
+		Validate.notBlank(name, "Field name is blank");
+		Validate.notBlank(type, "Field type is blank");
+
+		if (Constants.JAVA_KEYWORDS.contains(name)) {
+			throw new IllegalArgumentException("Field [" + name + "] is a Java keyword");
+		}
+
 		this.name = name;
 		this.upperName = StringUtils.capitalize(name);
 		this.type = type;
