057bb9e20b935febc1ac4fec6b885085f7aa0439
Jalen <jalen@kuzcolighting.com>
2016-09-05 22:21:50 -0700

    Move EhcacheItemConfig inside EhcacheConfig

--- itdoes-common/src/main/java/com/itdoes/common/extension/codegenerator/entity/EhcacheConfig.java
+++ itdoes-common/src/main/java/com/itdoes/common/extension/codegenerator/entity/EhcacheConfig.java
@@ -4,5 +4,11 @@ package com.itdoes.common.extension.codegenerator.entity;
  * @author Jalen Zhong
  */
 public class EhcacheConfig {
-
+	public class EhcacheItemConfig {
+		// private final String name;
+		// private final long maxEntriesLocalHeap;
+		// private final boolean eternal;
+		// private final boolean overflowToDisk;
+		// private final long maxEntriesLocalDisk;
+	}
 }
