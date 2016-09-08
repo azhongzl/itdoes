badf140bf57518d77680d91c4836442dbbda1619
Jalen <jalen@kuzcolighting.com>
2016-09-08 14:30:39 -0700

    Add util for comparing two doubles

--- itdoes-common/src/main/java/com/itdoes/common/core/util/Numbers.java
+++ itdoes-common/src/main/java/com/itdoes/common/core/util/Numbers.java
@@ -1,5 +1,7 @@
 package com.itdoes.common.core.util;
 
+import java.math.BigDecimal;
+
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
 
@@ -21,6 +23,12 @@ public class Numbers {
 
 	public static int bytesToInt(byte[] value) {
 		return Ints.fromByteArray(value);
+	}
+
+	public static int compareTo(double d1, double d2, int scale) {
+		final BigDecimal bd1 = new BigDecimal(d1).setScale(scale, BigDecimal.ROUND_HALF_UP);
+		final BigDecimal bd2 = new BigDecimal(d2).setScale(scale, BigDecimal.ROUND_HALF_UP);
+		return bd1.compareTo(bd2);
 	}
 
 	private Numbers() {
