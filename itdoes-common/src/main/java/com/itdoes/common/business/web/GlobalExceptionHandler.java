fb4bfe30625ff1e148551167b5cf305667374a9d
Jalen <jalen@kuzcolighting.com>
2016-09-09 01:18:58 -0700

    Enhance GlobleExceptionHandler to handle all exceptions

--- itdoes-common/src/main/java/com/itdoes/common/business/web/GlobalExceptionHandler.java
+++ itdoes-common/src/main/java/com/itdoes/common/business/web/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import javax.validation.ConstraintViolationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 
 import com.itdoes.common.core.Result;
 import com.itdoes.common.core.util.Validators;
@@ -23,24 +26,35 @@ import com.itdoes.common.core.web.MediaTypes;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	private static Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 	@ExceptionHandler(ConstraintViolationException.class)
-	public final ResponseEntity<?> handleException(ConstraintViolationException e, WebRequest request) {
+	public final ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException e,
+			WebRequest request) {
 		final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 		final Map<String, String> errors = Validators.propertyMessages(e);
 		final Result result = HttpResults.fail(httpStatus.value(), errors);
-		return handleExceptionInternal(e, result, newJsonHttpHeaders(), httpStatus, request);
+		return handleExceptionInternal(e, result, null, httpStatus, request);
 	}
 
-	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<?> handleException(RuntimeException e, WebRequest request) {
-		final HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-		final Result result = HttpResults.fail(httpStatus.value(), e.getMessage());
-		return handleExceptionInternal(e, result, newJsonHttpHeaders(), httpStatus, request);
-	}
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception e, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		LOGGER.error("Error occurred", e);
 
-	private static HttpHeaders newJsonHttpHeaders() {
-		final HttpHeaders headers = new HttpHeaders();
+		if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+			request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, e, WebRequest.SCOPE_REQUEST);
+		}
+
+		if (body == null) {
+			body = HttpResults.fail(status.value(), e.getMessage());
+		}
+
+		if (headers == null) {
+			headers = new HttpHeaders();
+		}
 		headers.setContentType(MediaType.parseMediaType(MediaTypes.APPLICATION_JSON_UTF_8));
-		return headers;
+
+		return new ResponseEntity<Object>(body, headers, status);
 	}
 }
