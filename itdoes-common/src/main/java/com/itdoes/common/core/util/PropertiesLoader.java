9121353b94569486fd66e9d178d68511680302c0
Jalen <jalen@kuzcolighting.com>
2016-09-12 12:47:43 -0700

    Support getting from more keys for PropertiesLoader

--- itdoes-common/src/main/java/com/itdoes/common/core/util/PropertiesLoader.java
+++ itdoes-common/src/main/java/com/itdoes/common/core/util/PropertiesLoader.java
@@ -2,6 +2,7 @@ package com.itdoes.common.core.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Properties;
 
@@ -68,6 +69,19 @@ public class PropertiesLoader {
 		return value == null ? defaultValue : value;
 	}
 
+	public String getProperty(String[] keys) {
+		String value = getValue(keys);
+		if (value == null) {
+			throw new NoSuchElementException("Keys " + Arrays.toString(keys) + " does not exist");
+		}
+		return value;
+	}
+
+	public String getProperty(String[] keys, String defaultValue) {
+		final String value = getValue(keys);
+		return value == null ? defaultValue : value;
+	}
+
 	public Integer getInteger(String key) {
 		final String value = getValue(key);
 		if (value == null) {
@@ -78,6 +92,19 @@ public class PropertiesLoader {
 
 	public Integer getInteger(String key, Integer defaultValue) {
 		final String value = getValue(key);
+		return value == null ? defaultValue : Integer.valueOf(value);
+	}
+
+	public Integer getInteger(String[] keys) {
+		final String value = getValue(keys);
+		if (value == null) {
+			throw new NoSuchElementException("Keys " + Arrays.toString(keys) + " does not exist");
+		}
+		return Integer.valueOf(value);
+	}
+
+	public Integer getInteger(String[] keys, Integer defaultValue) {
+		final String value = getValue(keys);
 		return value == null ? defaultValue : Integer.valueOf(value);
 	}
 
@@ -94,6 +121,19 @@ public class PropertiesLoader {
 		return value == null ? defaultValue : Long.valueOf(value);
 	}
 
+	public Long getLong(String[] keys) {
+		final String value = getValue(keys);
+		if (value == null) {
+			throw new NoSuchElementException("Keys " + Arrays.toString(keys) + " does not exist");
+		}
+		return Long.valueOf(value);
+	}
+
+	public Long getLong(String[] keys, Long defaultValue) {
+		final String value = getValue(keys);
+		return value == null ? defaultValue : Long.valueOf(value);
+	}
+
 	public Double getDouble(String key) {
 		final String value = getValue(key);
 		if (value == null) {
@@ -104,6 +144,19 @@ public class PropertiesLoader {
 
 	public Double getDouble(String key, Double defaultValue) {
 		final String value = getValue(key);
+		return value == null ? defaultValue : Double.valueOf(value);
+	}
+
+	public Double getDouble(String[] keys) {
+		final String value = getValue(keys);
+		if (value == null) {
+			throw new NoSuchElementException("Keys " + Arrays.toString(keys) + " does not exist");
+		}
+		return Double.valueOf(value);
+	}
+
+	public Double getDouble(String[] keys, Double defaultValue) {
+		final String value = getValue(keys);
 		return value == null ? defaultValue : Double.valueOf(value);
 	}
 
@@ -120,6 +173,19 @@ public class PropertiesLoader {
 		return value == null ? defaultValue : Boolean.valueOf(value);
 	}
 
+	public Boolean getBoolean(String[] keys) {
+		final String value = getValue(keys);
+		if (value == null) {
+			throw new NoSuchElementException("Keys " + Arrays.toString(keys) + " does not exist");
+		}
+		return Boolean.valueOf(value);
+	}
+
+	public Boolean getBoolean(String[] keys, Boolean defaultValue) {
+		final String value = getValue(keys);
+		return value == null ? defaultValue : Boolean.valueOf(value);
+	}
+
 	private String getValue(String key) {
 		if (systemPrefered) {
 			final String value = System.getProperty(key);
@@ -128,5 +194,16 @@ public class PropertiesLoader {
 			final String value = properties.getProperty(key);
 			return StringUtils.isBlank(value) ? System.getProperty(key) : value;
 		}
+	}
+
+	private String getValue(String[] keys) {
+		for (String key : keys) {
+			final String value = getValue(key);
+			if (value != null) {
+				return value;
+			}
+		}
+
+		return null;
 	}
 }
