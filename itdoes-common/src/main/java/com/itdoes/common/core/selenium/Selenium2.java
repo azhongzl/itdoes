c81b8917168ca00469882286a69f744dadfa3b55
Jalen <jalen@kuzcolighting.com>
2016-09-27 16:20:56 -0700

    Merge Selenium2

--- itdoes-common/src/main/java/com/itdoes/common/core/selenium/Selenium2.java
+++ itdoes-common/src/main/java/com/itdoes/common/core/selenium/Selenium2.java
@@ -200,11 +200,7 @@ public class Selenium2 {
 	public void typeEnter(By by) {
 		typeKeys(by, Keys.ENTER);
 	}
-	
-	public void typeEsc(By by) {
-		typeKeys(by, Keys.ESCAPE);
-	}
-	
+
 	public void typeReturn(By by) {
 		typeKeys(by, Keys.RETURN);
 	}
