11ec8f340e9f3f48d548da3da05716318abd13e1
Jalen <jalen@kuzcolighting.com>
2016-09-29 16:21:53 -0700

    Add initial search controller

--- itdoes-common/src/main/java/com/itdoes/common/business/service/SearchService.java
+++ itdoes-common/src/main/java/com/itdoes/common/business/service/SearchService.java
@@ -1,11 +1,18 @@
 package com.itdoes.common.business.service;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.lucene.search.Query;
 import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +20,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SearchService extends BaseService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SearchService.class);
+
 	@PersistenceContext
 	private EntityManager em;
 
@@ -24,7 +33,16 @@ public class SearchService extends BaseService {
 	}
 
 	public void createIndex() throws Exception {
-		// LOG.info("Updating Index");
+		LOGGER.info("Search engine index creating...");
 		ftem.createIndexer().startAndWait();
+		LOGGER.info("Search engine index created");
+	}
+
+	public List<?> search(Class<?> entityClass, String[] fields, String searchString) {
+		final QueryBuilder qb = ftem.getSearchFactory().buildQueryBuilder().forEntity(entityClass).get();
+		final Query q = qb.keyword().onFields(fields).matching(searchString).createQuery();
+		final FullTextQuery ftq = ftem.createFullTextQuery(q, entityClass);
+		final List<?> resultList = ftq.getResultList();
+		return resultList;
 	}
 }
