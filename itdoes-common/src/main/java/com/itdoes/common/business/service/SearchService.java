e3375ec0a023d6a36a931552e12f56dcfc3e8cbf
Jalen <jalen@kuzcolighting.com>
2016-09-29 22:27:52 -0700

    Add second version of SearchService

--- itdoes-common/src/main/java/com/itdoes/common/business/service/SearchService.java
+++ itdoes-common/src/main/java/com/itdoes/common/business/service/SearchService.java
@@ -1,11 +1,14 @@
 package com.itdoes.common.business.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.FullTextQuery;
@@ -38,11 +41,53 @@ public class SearchService extends BaseService {
 		LOGGER.info("Search engine index created");
 	}
 
-	public List<?> search(Class<?> entityClass, String[] fields, String searchString) {
-		final QueryBuilder qb = ftem.getSearchFactory().buildQueryBuilder().forEntity(entityClass).get();
-		final Query q = qb.keyword().onFields(fields).matching(searchString).createQuery();
-		final FullTextQuery ftq = ftem.createFullTextQuery(q, entityClass);
+	public List<?> search(String searchString, SearchEntity searchEntity, int firstResult, int maxResults) {
+		final Query q = createQuery(searchString, searchEntity);
+		final FullTextQuery ftq = ftem.createFullTextQuery(q, searchEntity.getEntityClass());
+		ftq.setFirstResult(firstResult);
+		ftq.setMaxResults(maxResults);
 		final List<?> resultList = ftq.getResultList();
 		return resultList;
+	}
+
+	public List<?> search(String searchString, List<SearchEntity> searchEntities, int firstResult, int maxResults) {
+		final List<Class<?>> entityClasses = new ArrayList<>(searchEntities.size());
+		final BooleanQuery.Builder bqb = new BooleanQuery.Builder();
+		for (SearchEntity searchEntity : searchEntities) {
+			final Query q = createQuery(searchString, searchEntity);
+			bqb.add(q, Occur.SHOULD);
+		}
+		final FullTextQuery ftq = ftem.createFullTextQuery(bqb.build(),
+				entityClasses.toArray(new Class[searchEntities.size()]));
+		ftq.setFirstResult(firstResult);
+		ftq.setMaxResults(maxResults);
+		final List<?> resultList = ftq.getResultList();
+		return resultList;
+	}
+
+	private Query createQuery(String searchString, SearchEntity searchEntity) {
+		final QueryBuilder queryBuilder = ftem.getSearchFactory().buildQueryBuilder()
+				.forEntity(searchEntity.getEntityClass()).get();
+		final Query query = queryBuilder.keyword().onFields(searchEntity.getFields()).matching(searchString)
+				.createQuery();
+		return query;
+	}
+
+	public static class SearchEntity {
+		private final Class<?> entityClass;
+		private final String[] fields;
+
+		public SearchEntity(Class<?> entityClass, String[] fields) {
+			this.entityClass = entityClass;
+			this.fields = fields;
+		}
+
+		public Class<?> getEntityClass() {
+			return entityClass;
+		}
+
+		public String[] getFields() {
+			return fields;
+		}
 	}
 }
