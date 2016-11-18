0ebd0a8d0007fb39df74bcb96418dae73da2321f
Jalen <jalen@kuzcolighting.com>
2016-11-18 13:34:23 -0800

    Add username and successUrl parameter for LoginController

--- itdoes-common/src/main/java/com/itdoes/common/business/web/LoginController.java
+++ itdoes-common/src/main/java/com/itdoes/common/business/web/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.itdoes.common.core.shiro.Shiros;
+
 /**
  * @author Jalen Zhong
  */
@@ -14,13 +16,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping(value = "/login")
 public class LoginController extends BaseController {
 	@RequestMapping(method = RequestMethod.GET)
-	public String login() {
+	public String login(
+			@RequestParam(value = FormAuthenticationFilter.DEFAULT_USERNAME_PARAM, required = false) String username,
+			@RequestParam(value = Shiros.SUCCESS_URL_KEY, required = false) String successUrl, Model model) {
+		model.addAttribute(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM, username);
+		model.addAttribute(Shiros.SUCCESS_URL_KEY, successUrl);
 		return "login";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String fail(@RequestParam(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM) String username, Model model) {
+	public String fail(
+			@RequestParam(value = FormAuthenticationFilter.DEFAULT_USERNAME_PARAM, required = false) String username,
+			@RequestParam(value = Shiros.SUCCESS_URL_KEY, required = false) String successUrl, Model model) {
 		model.addAttribute(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM, username);
+		model.addAttribute(Shiros.SUCCESS_URL_KEY, successUrl);
 		return "login";
 	}
 }
