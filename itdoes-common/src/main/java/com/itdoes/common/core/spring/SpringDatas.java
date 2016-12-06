f14fea9e7ba769c784ba83421737364fc601138a
Jalen <jalen@kuzcolighting.com>
2016-12-06 00:28:31 -0800

    Add SpringDatas

---
+++ itdoes-common/src/main/java/com/itdoes/common/core/spring/SpringDatas.java
@@ -0,0 +1,29 @@
+package com.itdoes.common.core.spring;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+/**
+ * @author Jalen Zhong
+ */
+public class SpringDatas {
+	public static PageRequest newPageRequest(int pageNo, int pageSize, int maxPageSize, Sort sort) {
+		if (pageNo < 1) {
+			pageNo = 1;
+		}
+
+		if (pageSize < 1 || pageSize > maxPageSize) {
+			pageSize = maxPageSize;
+		}
+
+		return new PageRequest(pageNo - 1, pageSize, sort);
+	}
+
+	public static Sort newSort(boolean asc, String field) {
+		return new Sort(asc ? Direction.ASC : Direction.DESC, field);
+	}
+
+	private SpringDatas() {
+	}
+}
