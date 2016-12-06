1adeb6eab284da4c64b94bad4abdd8accf754e91
Jalen <jalen@kuzcolighting.com>
2016-12-05 21:05:31 -0800

    Add oldEntity key

--- itdoes-common/src/main/java/com/itdoes/common/business/web/BaseEntityPutController.java
+++ itdoes-common/src/main/java/com/itdoes/common/business/web/BaseEntityPutController.java
@@ -8,15 +8,18 @@ import com.itdoes.common.core.cglib.CglibMapper;
  * @author Jalen Zhong
  */
 public abstract class BaseEntityPutController extends BaseEntityController {
+	private static final String ENTITY_KEY = "entity";
+	private static final String OLD_ENTITY_KEY = "oldEntity";
+
 	protected void cacheEntity(ServletRequest request, Object entity) {
-		request.setAttribute("entity", entity);
+		request.setAttribute(ENTITY_KEY, entity);
 
 		final Object oldEntity = CglibMapper.copy(entity);
-		request.setAttribute("oldEntity", oldEntity);
+		request.setAttribute(OLD_ENTITY_KEY, oldEntity);
 	}
 
 	@SuppressWarnings("unchecked")
 	protected <T> T getOldEntity(ServletRequest request) {
-		return (T) request.getAttribute("oldEntity");
+		return (T) request.getAttribute(OLD_ENTITY_KEY);
 	}
 }
