1ab6f654bf098eb4e8a2ff2988c8764d90fb7bad
Jalen <jalen@kuzcolighting.com>
2016-12-06 19:33:28 -0800

    Add MapModel

---
+++ itdoes-common/src/main/java/com/itdoes/common/core/MapModel.java
@@ -0,0 +1,10 @@
+package com.itdoes.common.core;
+
+import java.util.HashMap;
+
+/**
+ * @author Jalen Zhong
+ */
+public class MapModel extends HashMap<Object, Object> {
+	private static final long serialVersionUID = -8078539341799761187L;
+}
