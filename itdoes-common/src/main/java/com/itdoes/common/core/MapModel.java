d7f9ad2af32c9ede6877bbe776a0072c764c8585
Jalen <jalen@kuzcolighting.com>
2016-12-06 19:40:11 -0800

    Add empty MapModel

--- itdoes-common/src/main/java/com/itdoes/common/core/MapModel.java
+++ itdoes-common/src/main/java/com/itdoes/common/core/MapModel.java
@@ -7,4 +7,10 @@ import java.util.HashMap;
  */
 public class MapModel extends HashMap<Object, Object> {
 	private static final long serialVersionUID = -8078539341799761187L;
+
+	private static final MapModel EMPTY_MAP_MODEL = new MapModel();
+
+	public static MapModel emptyMapModel() {
+		return EMPTY_MAP_MODEL;
+	}
 }
