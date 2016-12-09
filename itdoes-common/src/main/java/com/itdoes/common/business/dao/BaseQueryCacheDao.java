432d7196b70fe72557c9010240176619ae5674dd
Jalen <jalen@kuzcolighting.com>
2016-12-08 21:56:50 -0800

    Allow all findAll() for query cache

--- itdoes-common/src/main/java/com/itdoes/common/business/dao/BaseQueryCacheDao.java
+++ itdoes-common/src/main/java/com/itdoes/common/business/dao/BaseQueryCacheDao.java
@@ -1,11 +1,13 @@
 package com.itdoes.common.business.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.QueryHint;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.QueryHints;
 
@@ -15,4 +17,10 @@ import org.springframework.data.jpa.repository.QueryHints;
 public interface BaseQueryCacheDao<T, ID extends Serializable> extends BaseDao<T, ID> {
 	@QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
 	Page<T> findAll(Specification<T> spec, Pageable pageable);
+
+	@QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
+	List<T> findAll(Specification<T> spec, Sort sort);
+
+	@QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
+	List<T> findAll(Specification<T> spec);
 }
